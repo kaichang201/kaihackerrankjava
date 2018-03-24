4aa33425dc4d0f229d11a2d7477eb016aa8f4fa2
kaichang201 <kaichang201@gmail.com>
2018-03-24 17:18:49 -0400

    initial left rotation

---
+++ src/org/kai/leftrotation.java
@@ -0,0 +1,29 @@
+package org.kai;
+
+import java.util.Scanner;
+
+public class leftrotation {
+
+    private static final Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] nd = scan.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0].trim());
+
+        int d = Integer.parseInt(nd[1].trim());
+
+        int[] a = new int[n];
+
+        String[] aItems = scan.nextLine().split(" ");
+
+        for (int aItr = 0; aItr < n; aItr++) {
+            int aItem = Integer.parseInt(aItems[aItr].trim());
+            a[aItr] = aItem;
+        }
+        
+        for (int i = 0; i <n ;i++) {
+        	System.out.print(a[i]);
+        }
+    }
+}
