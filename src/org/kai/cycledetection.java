99cf23885995d282b5109239d29bc6ad58c26613
kaichang201 <kaichang201@gmail.com>
2018-03-28 20:31:29 -0400

    5 points.. ugh..

---
+++ src/org/kai/cycledetection.java
@@ -0,0 +1,17 @@
+package org.kai;
+
+import java.util.Scanner;
+
+public class cycledetection {
+
+	boolean hasCycle(Node head) {
+	    Node ptr1 = head, ptr2 = head;
+	    while (true) {
+	        if (ptr1 == null || ptr2==null || ptr1.next == null || ptr2.next == null ||ptr2.next.next == null) return false;
+	        ptr1 = ptr1.next;
+	        ptr2 = ptr2.next.next;
+	        if (ptr1 == ptr2) return true;
+	    }
+	}
+
+}
