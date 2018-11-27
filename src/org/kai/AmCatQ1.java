ca0e11b82dd8b28c9f4d4e1246c934ef0df23d15
Kai <kaichang201@gmail.com>
2018-11-26 23:57:11 -0500

    Amazon Interview Questions.  Passed Q1. Failed Q2.  Took a little longer to debug, but I finished it here.

---
+++ src/org/kai/AmCatQ1.java
@@ -0,0 +1,94 @@
+package org.kai;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class AmCatQ1 {
+
+
+
+public static void main(String[] args) {
+/*
+ * Test ID 23280666774313
+ * Test ID 23280666774313
+ * 
+ * Removed question statement out of respect for Amazon interviewing process
+ * 
+ * Example
+ *: Input num destinations 3
+ * allocations {1,2},{3,4}{1,-1}}
+ * numDeliveries = 2
+ * Output [ {1,-1}, {1,2}}
+ * 22 out of 22 passed.
+ */
+
+
+	AmCatQ1 me = new AmCatQ1();
+	
+	int[] testcase1 = {{1,2},{3,4}{1,-1}};
+
+
+	
+	long startTime = System.currentTimeMillis();
+
+	System.out.println(me.ClosestXdestinations(3,2)); //  
+
+
+	System.out.println("Time taken " + (System.currentTimeMillis() - startTime));
+	
+
+}
+
+
+
+	public List<List<Integer>> ClosestXdestinations(int numDestinations, List<List<Integer>> allLocations, int numDeliveries) {
+		/*
+		 * Strategy; example explanation seems to imply the truck always returns to warehouse before each delivery.  so distance
+		 * always calculated from [0,0]. Let's try this way.
+		 * loop across allLocations.  put into a priorityqueue that has custom comparator to calculate distance from 0,0
+		 * then pull numDeliveries from pq
+		 */
+		List<List<Integer>> rv = new ArrayList<>();
+		Comparator<List<Integer>> myComparator = new Comparator<List<Integer>>() {
+			@Override
+			public int compare (List<Integer> d1, List<Integer> d2) {
+				double distance1 = Math.sqrt(Math.pow(d1.get(0),2) + Math.pow(d1.get(1),2));
+				double distance2 = Math.sqrt(Math.pow(d2.get(0),2) + Math.pow(d2.get(1),2));
+				if (distance1 > distance2)
+					return 1;
+				if (distance1 < distance2)
+					return -1;
+				return 0;							
+						
+			}
+		};
+		PriorityQueue<List<Integer>> pq = new PriorityQueue<> (10, myComparator);
+		
+		// loop across all locations.  add to dq and let it do the sorting
+		for (List<Integer> d: allLocations)
+			pq.add(d);
+		
+		// pull up the num of Deliveries
+		for (int i = 0; i<numDeliveries; i++)
+			rv.add(pq.poll());
+		return rv;
+	}
+	
+
+
+}
---
+++ src/org/kai/AmCatQ2.java
@@ -0,0 +1,168 @@
+package org.kai;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.kai.LeetCodeNumberOfIslands.Node;
+
+
+public class AmCatQ2 {
+
+
+
+public static void main(String[] args) {
+/*
+ * Removed question statement out of respect for Amazon interviewing process
+ *  
+ *  Example
+ *  Input numRows 3, numColumns 3
+ *  lot = {{1,0,0},{1,0,0},{1,9,1}}
+ *  
+ *  Output:
+ *  3
+ *  Explanation:
+ *  Starting from top left corner, the demolition bot traverse cells {0,0},{1,0},{2,0}->{2,1}
+ *  the robot traversed 3 distance, so output is 3.
+ * 
+ */
+
+
+	AmCatQ2 me = new AmCatQ2();
+	
+	Integer[][] testcase1 = {{1,0,0},{1,0,0},{1,9,1}};
+	List<List<Integer>> t1 = new ArrayList<>();
+	for (Integer[] arr: testcase1)
+		t1.add(Arrays.asList(arr));
+
+
+	Integer[][] testcase2 = { {1,1,1,1},
+							  {0,1,1,1}
+							 ,{0,1,0,1},
+							  {1,1,9,1},
+							  {0,0,1,1}};	
+	List<List<Integer>> t2 = new ArrayList<>();
+	for (Integer[] arr: testcase2)
+		t2.add(Arrays.asList(arr));
+
+	
+	Integer[][] testcase3 = { {1,1,1,1},
+			 				  {0,1,1,1},
+			 				  {0,1,0,1},
+			 				  {1,0,9,0},
+			 				  {0,1,0,1}};
+	List<List<Integer>> t3 = new ArrayList<>();
+	for (Integer[] arr: testcase3)
+		t3.add(Arrays.asList(arr));
+
+
+
+	long startTime = System.currentTimeMillis();
+
+	System.out.println(me.removeObstacle(3,3,t1)); //  
+	System.out.println(me.removeObstacle(5,4,t2)); //  
+	System.out.println(me.removeObstacle(5,4,t3)); //  
+
+	System.out.println("Time taken " + (System.currentTimeMillis() - startTime));
+	
+}
+
+	class Node {
+		int x;
+		int y;
+		Node (int x, int y) {
+			this.x=x;
+			this.y=y;
+		}
+		@Override
+		public boolean equals (Object inNode) {
+			if (inNode instanceof Node) {
+				return (((Node) inNode).x==this.x  && ((Node)inNode).y == this.y);
+			}
+			return false;
+		}
+		@Override
+		public int hashCode() { // hash code is necessary for hash set to function properly
+			int prime = 13;
+			return x*prime + y;
+		}
+		@Override
+		public String toString () {
+			return x + "," +y;
+		}
+	}
+
+	public int removeObstacle(int numRows, int numColumns, List<List<Integer>> lot)
+	{
+		/*
+		 * Strategy:convert from List< BFS from 0,0
+		 * until value is 9
+		 */
+	
+		int[][] map = new int[numRows][numColumns];
+		for (int i = 0; i< numRows; i++) 
+			for (int j = 0; j<numColumns; j++)
+				map[i][j] = lot.get(i).get(j);
+
+		int steps = 0;
+		int currentCount=1, childCount=0;
+		Deque<Node> q = new ArrayDeque<>();
+		boolean[][] visited = new boolean[numRows][numColumns];
+
+		q.add(new Node(0,0));
+		while (!q.isEmpty()) {
+			if (currentCount==0) { // I've processed everything in this level. next pop is next level.
+				steps++;
+				currentCount = childCount;
+				childCount=0;
+			}
+			
+			Node n = q.pop();
+			currentCount--;
+			visited[n.x][n.y] = true;
+			//System.out.println("Processing x "+ n.x + " y " + n.y);
+			if (map[n.x][n.y] == 9)
+				return steps;
+			
+			// add the next generation to be visited 
+			// look up
+			if(n.x != 0 && !visited[n.x-1][n.y] && map[n.x-1][n.y] != 0) {
+				q.add(new Node(n.x-1, n.y));
+				childCount++;
+			}
+			// look down
+			if(n.x < numRows-1 && !visited[n.x+1][n.y] && map[n.x+1][n.y] !=0) {
+				q.add(new Node(n.x+1, n.y));
+				childCount++;
+			}
+			// look left
+			if(n.y != 0 && !visited[n.x][n.y-1] && map[n.x][n.y-1] != 0) {
+				q.add(new Node(n.x, n.y-1));
+				childCount++;
+			}
+			// look right
+			if(n.y < numColumns-1 && !visited[n.x][n.y+1] && map[n.x][n.y+1] != 0) {
+				q.add(new Node(n.x, n.y+1));
+				childCount++;
+			}
+
+
+		}
+		
+
+		return -1;
+	}
+
+
+}
